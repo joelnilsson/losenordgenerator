92055a2d5bbb7548b97651589a9ad58708dec5d1
Joel Nilsson <joel.nilsson9@learnet.se>
2019-11-26 09:00:48 +0100

    lösenordsgenerator klar

---
+++ losenordsgenerator.py
@@ -0,0 +1,19 @@
+chars = "abcdefghijklmnopqrstuvwxyz"
+chars1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+chars2 = "1234567890"
+chars3 = "!%&/()=#"
+import random 
+import string
+password = ""
+password = ""
+for c in range (2):
+    password += random.choice(chars)
+    password += random.choice(chars1)
+    password += random.choice(chars2)
+    password += random.choice(chars3)
+
+password = ''.join(random.sample(password,len(password)))
+
+f= open("losenordfil.txt","a") 
+f.write(password)
+f.write("\n")
