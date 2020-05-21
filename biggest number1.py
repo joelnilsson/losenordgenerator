4c703f6b8b51c062893836b09a58765f5981ed57
Joel Nilsson <joel.nilsson9@learnet.se>
2020-05-21 10:56:37 +0200

    Nu är jag klar med kommentarerna

--- biggest number1.py
+++ biggest number1.py
@@ -1,12 +1,12 @@
 störstaTal = 0
-lista = []
+lista = [] # Skapar en lista där sifforrna sparas
 print("Hur många siffror finns i listan?")
-antal = int(input(""))
-for x in range(antal):
-    tal = int(input("Skriv ett tal: "))
-    lista.append(tal)
+antal = int(input("")) # Skriver hur många tal det ska vara i listan
+for x in range(antal): # Kör koden "x" antal gånger
+    tal = int(input("Skriv ett tal: ")) # Skriver in talen
+    lista.append(tal) #Lägger till talet som skrivits i listan
 print(lista)
 for g in lista:
-    if g > störstaTal:
-        störstatalet = g
+    if g > störstaTal: # Kör alla tal
+        störstatalet = g # Om g är större än störstatalet
 print("största talet är: ", störstaTal)
